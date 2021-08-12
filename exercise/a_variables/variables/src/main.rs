a7bbe602c4d5aa5e92b970d17664a246fa0e8bba
Alex Palmer <xanderx@xanderx.com>
2021-08-12 19:00:40 +0100

    Exercise A extra: Explicitly annotate variable types

--- exercise/a_variables/variables/src/main.rs
+++ exercise/a_variables/variables/src/main.rs
@@ -2,8 +2,8 @@ const STARTING_MISSLES: i32 = 8;
 const READY_AMOUNT: i32 = 2;
 
 fn main() {
-    let mut missiles = STARTING_MISSLES;
-    let ready = READY_AMOUNT;
+    let mut missiles: i32 = STARTING_MISSLES;
+    let ready: i32 = READY_AMOUNT;
     println!("Firing {} of my {} missiles...", ready, missiles);
 
     missiles = missiles - ready;
