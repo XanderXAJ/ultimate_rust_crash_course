4dc933aeab5d7aae9b76baa50b09e580d3a6fa8c
Alex Palmer <xanderx@xanderx.com>
2021-08-12 19:02:00 +0100

    Exercise A Extra: Perform subtraction inline with println

--- exercise/a_variables/variables/src/main.rs
+++ exercise/a_variables/variables/src/main.rs
@@ -2,9 +2,8 @@ const STARTING_MISSLES: i32 = 8;
 const READY_AMOUNT: i32 = 2;
 
 fn main() {
-    let (mut missiles, ready): (i32, i32) = (STARTING_MISSLES, READY_AMOUNT);
+    let (missiles, ready): (i32, i32) = (STARTING_MISSLES, READY_AMOUNT);
     println!("Firing {} of my {} missiles...", ready, missiles);
 
-    missiles = missiles - ready;
-    println!("{} missiles left", missiles);
+    println!("{} missiles left", missiles - ready);
 }
